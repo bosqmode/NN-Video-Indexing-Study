3fa3bacdc242ed6a2259ac382a1a7d22bc47f5a5
bosqmode <joel.pesu1@gmail.com>
2022-05-16 23:07:25 +0300

    labeling service dev

---
+++ MappingService/images/labelling/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+while :
+do
+    python3 /label.py
+    sleep 60
+done
---
+++ MappingService/images/labelling/label.py
@@ -0,0 +1,7 @@
+# import tensorflow as tf
+
+# print(tf.__version__)
+
+f = open("demofile2.txt", "a")
+f.write("Now the file has more content!")
+f.close()
